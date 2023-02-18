1ad758b1bb8c8d826ff5d97963f3b7a9914da780
Ryan O'Kelley <ryan.okelley@gmail.com>
2023-02-18 16:19:29 -0500

    Bind to

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from os import environ
 
 app = Flask(__name__)
 
@@ -7,4 +8,7 @@ def home():
     return render_template('./index.html')
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(
+        debug=False
+        port=environ.get('PORT')
+    )

612689011f523dd14f8ff030e98287ae2434e9ec
Ryan O'Kelley <ryan.okelley@gmail.com>
2023-02-18 16:23:30 -0500

    Bind to any interface

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def home():
     return render_template('./index.html')
 
 if __name__ == '__main__':
-    app.run(debug=False, port=environ.get('PORT')
+    app.run(debug=False, host='0.0.0.0', port=environ.get('PORT')
     )

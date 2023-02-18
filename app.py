17f9b0f79c244201a6be5750931b10321229ce92
Ryan O'Kelley <ryan.okelley@gmail.com>
2023-02-18 16:26:26 -0500

    Correct path to home

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('./index.html')
+    return render_template('./static/index.html')
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=environ.get('PORT')

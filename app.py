bc45ad825c7e627bf451fb3491c63a2c703de782
Ryan O'Kelley <ryan.okelley@gmail.com>
2023-02-18 16:20:25 -0500

    Fix run syntax

--- app.py
+++ app.py
@@ -8,7 +8,5 @@ def home():
     return render_template('./index.html')
 
 if __name__ == '__main__':
-    app.run(
-        debug=False
-        port=environ.get('PORT')
+    app.run(debug=False, port=environ.get('PORT')
     )
